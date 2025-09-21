02f11bed55ae217844a95f9c96bd39833e41e234
ashwini <ashwiniswami1995@gmail.com>
2025-09-21 17:51:33 +0530

    feature commit

--- Assettest/src/test/java/feature89.java
+++ Assettest/src/test/java/feature89.java
@@ -4,6 +4,7 @@ public class feature89 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("feature 123 class");
+		System.out.println("feature 2 class");
 
 	}
 
