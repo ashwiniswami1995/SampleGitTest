6c9967b4e304f49470119c40102fa9ca31b2adf0
ashwiniswami1995 <ashwiniswami1995@gmail.com>
2025-09-21 16:23:31 +0530

    Update softtest.java

--- Assettest/src/test/java/softasserttest/softtest.java
+++ Assettest/src/test/java/softasserttest/softtest.java
@@ -6,17 +6,17 @@ public class softtest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		SoftAssert soft = new SoftAssert();
-		System.out.println("Step 1");
-		soft.assertTrue(false, "not true");
-		System.out.println("Step 2");
-		soft.assertEquals("ash", "ash2", "not equal");
-		System.out.println("Step 3");
-		soft.assertNull(null, "not equal");
-     System.out.println("Hi");
+		//SoftAssert soft = new SoftAssert();
+	//	System.out.println("Step 1");
+		//soft.assertTrue(false, "not true");
+		//System.out.println("Step 2");
+	//	soft.assertEquals("ash", "ash2", "not equal");
+	//	System.out.println("Step 3");
+	//	soft.assertNull(null, "not equal");
+         System.out.println("Hi");
 
 		// Must call at the end, otherwise failures won't be reported
-		soft.assertAll();
+		//soft.assertAll();
 	}
 
 }
