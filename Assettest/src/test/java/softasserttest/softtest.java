81a31686b0974d9f8bd52a6ea43fe47483458208
ashwini <ashwiniswami1995@gmail.com>
2025-09-21 22:56:51 +0530

    feature 123 statment

--- Assettest/src/test/java/softasserttest/softtest.java
+++ Assettest/src/test/java/softasserttest/softtest.java
@@ -13,8 +13,8 @@ public class softtest {
 	//	soft.assertEquals("ash", "ash2", "not equal");
 	//	System.out.println("Step 3");
 	//	soft.assertNull(null, "not equal");
-     System.out.println("Hi");
-     System.out.println("Hello");
+     System.out.println("atishfeature1234");
+
 		// Must call at the end, otherwise failures won't be reported
 		//soft.assertAll();
 	}
