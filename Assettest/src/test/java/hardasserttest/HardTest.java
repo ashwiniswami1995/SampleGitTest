35b6747afab825308a47ef42e88ce222705e2c3a
ashwini <ashwiniswami1995@gmail.com>
2025-09-21 15:37:15 +0530

    This is sample

---
+++ Assettest/src/test/java/hardasserttest/HardTest.java
@@ -0,0 +1,20 @@
+package hardasserttest;
+
+import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
+
+public class HardTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s1 ="ash";
+String s2 ="ash";
+
+		//Assert.assertEquals("ash", "ash1" ,"not equals");
+		//Assert.assertNotEquals("ash1", "ash1" ," equals");
+		//Assert.assertTrue( s1.equals(s2),"not equal");
+		//Assert.assertFalse( s1.equals(s2),"not equal");
+
+	}
+
+}
---
+++ Assettest/src/test/java/softasserttest/softtest.java
@@ -0,0 +1,21 @@
+package softasserttest;
+
+import org.testng.asserts.SoftAssert;
+
+public class softtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SoftAssert soft = new SoftAssert();
+		System.out.println("Step 1");
+		soft.assertTrue(false, "not true");
+		System.out.println("Step 2");
+		soft.assertEquals("ash", "ash2", "not equal");
+		System.out.println("Step 3");
+		soft.assertNotNull(null, "not equal");
+
+		// Must call at the end, otherwise failures won't be reported
+		soft.assertAll();
+	}
+
+}
